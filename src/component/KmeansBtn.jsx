ee6b856f398d378e6c239ccee4c0bf2c8bbfbfe1
andythebreaker <e24074724@mail.ncku.edu.tw>
2022-06-22 16:18:57 +0800

    typo

--- src/component/KmeansBtn.jsx
+++ src/component/KmeansBtn.jsx
@@ -391,13 +391,13 @@ S     P S       S S  S  S S    S S      S       S S    O S     P
 S    S   S     S   S  S S S    S S       S     S  S    O S    S  
 P ss"     "sss"     "ss"S P    P P sSSs   "sss"   P    P P ss"
                   `);
-                  console.log($(e.target).closest('.ui.compact.message').text());
-                  console.log($(e.target).closest('.ui.compact.message'));
-                  console.log($(e.target)); 
+                console.log($(e.target).closest('.ui.compact.message').text());
+                console.log($(e.target).closest('.ui.compact.message'));
+                console.log($(e.target));
                 console.log(e.target);
                 var downloadcanvasTMP = document.createElement('a');
                 downloadcanvasTMP.href = this.canvasA.toDataURL("image/png");
-                downloadcanvasTMP.download = $(e.target).closest('.ui.compact.message').text() + "_downloadcanvasT_" + String(makeid(7)) + ".png";
+                downloadcanvasTMP.download = $(e.target).closest('.TLABLFD').text() + "_downloadcanvasT_" + String(makeid(7)) + ".png";
                 document.body.appendChild(downloadcanvasTMP);
                 downloadcanvasTMP.click();
                 document.body.removeChild(downloadcanvasTMP);
