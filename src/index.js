e7fe79c101429bf1fb768c0805b0a70ee8101e0e
andythebreaker <e24074724@mail.ncku.edu.tw>
2024-04-20 22:58:43 +0800

    2.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Header } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header as='h1'>蘇小羽</Header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
