987ce1cd267d9f8c17ee9aceee71a975ce9ea4d7
andythebreaker <e24074724@mail.ncku.edu.tw>
2024-04-20 23:02:26 +0800

    理解了

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import BearCounter from './components/BearCounter';
 import Controls from './components/Controls';
+import { Header } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div>
+      <Header as='h2'>林+霓</Header>
       <h1>Welcome to My Bear Kingdom</h1>
       <BearCounter />
       <Controls />
