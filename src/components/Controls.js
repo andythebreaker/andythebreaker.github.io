2a576b3fa4b4a5392534559519682bdd915a78b2
andythebreaker <e24074724@mail.ncku.edu.tw>
2024-04-20 23:33:48 +0800

    setBearNumber

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -4,10 +4,13 @@ import { useBearStore } from '../stores/useBearStore';
 function Controls() {
   // Using Zustand hook to access the increasePopulation action from the store
   const increasePopulation = useBearStore(state => state.increasePopulation);
-
+  const removeAllBears = useBearStore(state => state.removeAllBears);
+  const setBearNumber=useBearStore(state=>state.setBearNumber);
   return (
     <div>
       <button onClick={increasePopulation}>One up</button>
+      <button onClick={removeAllBears}>zero</button>
+      <button onClick={()=>{setBearNumber(48)}}>set48</button>
     </div>
   );
 }
--- src/stores/useBearStore.js
+++ src/stores/useBearStore.js
@@ -1,9 +1,10 @@
-import create from 'zustand';
+import {create} from 'zustand';
 
 // Define and export the Zustand store
 export const useBearStore = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
+  setBearNumber: (number) => set({ bears: number }), // New function to set bear number
 }));
 
