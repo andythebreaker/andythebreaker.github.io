ec868b7e697832be6ea3de2e3d2f2b65e57e4c14
andythebreaker <e24074724@mail.ncku.edu.tw>
2024-04-25 00:03:46 +0800

    remove bearconter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import BearCounter from './components/BearCounter';
-import Controls from './components/Controls';
 import { Header } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
@@ -10,9 +8,6 @@ function App() {
   return (
     <div>
       <Header as='h2'>林+霓</Header>
-      <h1>Welcome to My Bear Kingdom</h1>
-      <BearCounter />
-      <Controls />
       <MainDisplay></MainDisplay>
     </div>
   );
